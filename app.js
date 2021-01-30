475f98a4ae588e54e76714f6a3cf68104e8b6b46
Mandi <haase1020@gmail.com>
2021-01-30 18:56:59 -0500

    update files

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.send("<p>home page</p>");
+  //   res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.get("/about", (req, res) => {
